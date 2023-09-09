f0ef6f317563e13ebfdc3322bf4169a3f23ac127
[VladislavkiSelov] <[vladislavkiselev.2007@gmail.com]>
2023-09-09 17:08:16 +0300

    add link

--- js/script.js
+++ js/script.js
@@ -1,5 +1,9 @@
 import '../scss/style.scss';
-
+const imgArray = Array.from(document.querySelectorAll('img'));
+imgArray.forEach((element) => {
+    element.setAttribute('src', `https://vladislavkiselov.github.io/Gastaway/${element.getAttribute('src')}`)
+    // element.getAttribute('src')
+})
 const burger = document.querySelector('.burger_header')
 burger.addEventListener('click', () => {
     const navigation = document.querySelector('.navigation')
@@ -7,7 +11,7 @@ burger.addEventListener('click', () => {
         navigation.style.display = "none";
         burger.classList.remove('close_burger')
         return
-    } 
+    }
     navigation.style.display = "block"
     burger.classList.add('close_burger')
 })
