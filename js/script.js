7acf2275a07928a7b0b6f7387e5de3a3248750b6
[VladislavkiSelov] <[vladislavkiselev.2007@gmail.com]>
2023-09-09 14:58:02 +0300

    add mobile table

--- js/script.js
+++ js/script.js
@@ -1,1 +1,13 @@
-import '../scss/style.scss'
+import '../scss/style.scss';
+
+const burger = document.querySelector('.burger_header')
+burger.addEventListener('click', () => {
+    const navigation = document.querySelector('.navigation')
+    if (navigation.style.display === "block") {
+        navigation.style.display = "none";
+        burger.classList.remove('close_burger')
+        return
+    } 
+    navigation.style.display = "block"
+    burger.classList.add('close_burger')
+})
